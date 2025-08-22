979e96860ad348165eadbda088e36c476011eb8c
Nicola <61830443+nicola02nb@users.noreply.github.com>
2025-08-22 15:10:07 +0200

    Refactor CSP policies in betterTTS plugin to use array syntax

--- plugins/betterTTS/native.ts
+++ plugins/betterTTS/native.ts
@@ -1,4 +1,10 @@
-import { CspPolicies, MediaSrc } from "@main/csp";
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
-CspPolicies["api.streamelements.com"] = MediaSrc;
-CspPolicies["tiktok-tts.weilnet.workers.dev"] = MediaSrc;
+import { CspPolicies } from "@main/csp";
+
+CspPolicies["api.streamelements.com"] = ["media-src"];
+CspPolicies["tiktok-tts.weilnet.workers.dev"] = ["media-src"];
